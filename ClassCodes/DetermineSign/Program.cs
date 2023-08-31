350311525be3167fac7f43498a427b4ca8166ab0
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:03:51 -0700

    this code will tell you if your num is + or -

---
+++ ClassCodes/DetermineSign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetermineSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number so we can tell you its Sign");
+            int Entered = Convert.ToInt32(Console.ReadLine());
+            if (Entered >= 0)
+                Console.WriteLine($"{Entered} Is Positive (+)");
+            else
+                Console.WriteLine($"{Entered} Is Negetive (-)");
+
+        }
+    }
+}
