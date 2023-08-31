193fd982802b26020aaf2b8b6fb02c29428395e4
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:20:21 -0700

    this code will determine the min,max and the average of 3 input numbers

---
+++ ClassCodes/MinMaxAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Array.Sort(new int[] { num1, num2, num3 });
+            Console.WriteLine($"{(new int[] { num1, num2, num3 })[0]} Is Our Minimum Number");
+            Console.WriteLine($"{(new int[] { num1, num2, num3 })[2]} Is Our Maximum Number");
+            Console.WriteLine($"{((new int[] { num1, num2, num3 })[0] + (new int[] { num1, num2, num3 })[1] + (new int[] { num1, num2, num3 })[2]) / 3} Is The Average");
+
+        }
+    }
+}
