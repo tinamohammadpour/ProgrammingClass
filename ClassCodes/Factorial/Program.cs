eed869463e9c7454576c178c72f176c02e633eb4
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:06:43 -0700

    this added code will compute the input number factorial for you

---
+++ ClassCodes/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long Factorial(int num)
+            {
+                long Fact = 1;
+                for (int i = 1; i <= num; i++)
+                    Fact = Fact * i;
+                return Fact;
+            }
+
+            Console.WriteLine("Enter your Number So i Compute Its Factorial");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(Number));
+        }
+    }
+}
