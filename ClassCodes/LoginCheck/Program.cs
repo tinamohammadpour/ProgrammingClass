714d930f1ba734e439ac78e5abbd0f4a24fd91d2
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:17:59 -0700

    this code will tell you if your login was seccessfull or not

---
+++ ClassCodes/LoginCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DefaultUserName = "User";
+            const string DefaultPassword = "Pass";
+            Console.WriteLine("Enter your UserName");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("Enter Your Pawssword");
+            string Password = Console.ReadLine();
+            UserName.Trim();
+            Password.Trim();
+            if (UserName.Equals(DefaultUserName) && Password.Equals(DefaultPassword))
+            {
+                Console.WriteLine("Login Successfull");
+            }
+            else
+                Console.WriteLine("Login Failed");
+        }
+    }
+}
