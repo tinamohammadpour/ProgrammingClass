d2e932fe97479d7e5ee0289599b2727726c00349
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:16:23 -0700

    this code will count the number of the input charecter in our text

---
+++ ClassCodes/IterationOfChar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationOfChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Text");
+            string Text = Console.ReadLine();
+            Console.WriteLine("Enter The Charecter You Are Looking For");
+            char InputChar = Convert.ToChar(Console.ReadLine());
+            int CharCounter = 0;
+            for (int i = 0; i <= Text.Length - 1; i++)
+            {
+                if (Text[i] == Char.ToUpper(InputChar) || Text[i] == Char.ToLower(InputChar))
+                    CharCounter++;
+            }
+            Console.WriteLine($"The Number of {InputChar} in the Text Is:{CharCounter}");
+
+        }
+    }
+}
