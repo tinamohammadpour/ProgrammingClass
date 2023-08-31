86b7511ac7be43a799a904066ca8fd6b8e67b85f
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 12:46:08 -0700

    the code for addeing an input char to the chars of a string hass been added

---
+++ ClassCodes/CharBetweenStr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharBetweenStr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write your Input string");
+            string InputWord = Console.ReadLine();
+            Console.WriteLine("write your Input Charecter you want to type between the chars of your string");
+            char InputChar = Convert.ToChar(Console.ReadLine());
+            for (int i = 0; i <= InputWord.Length - 1; i++)
+                Console.Write($"{InputWord[i]} {InputChar}");
+        }
+    }
+}
