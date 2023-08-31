f44306aea7485ff8b632d89a5c0646ff8e25539a
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 12:47:52 -0700

    the code for computing the benefit has been added

---
+++ ClassCodes/ComputeBenefit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeBenefit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double CountProfit(int Rial, double Profit, int Month)
+            {
+                double Result = Math.Pow((100 + Profit) / 100, Month) * Rial;
+                return Result;
+            }
+            Console.WriteLine("Enter the Money Amount");
+            int rial = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the profit percent");
+            double profit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Month");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(CountProfit(rial, profit, month));
+        }
+    }
+}
