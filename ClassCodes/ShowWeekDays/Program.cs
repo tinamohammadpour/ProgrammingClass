5cf7e64de8ae305a8f355cd0a02689343f3f6137
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:35:15 -0700

    this added code will show you the nth day of week with entering n

---
+++ ClassCodes/ShowWeekDays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowWeekDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string DayCounter;
+            Console.WriteLine("Choose a Number from 1 to 7 So We Can Show You Week Days");
+            Console.WriteLine("Enter \"0\" if You want To Exit");
+            while (true)
+            {
+                DayCounter = Console.ReadLine();
+                if (DayCounter == "1")
+                    Console.WriteLine("SaturDay");
+                else if (DayCounter == "2")
+                    Console.WriteLine("SunDay");
+                else if (DayCounter == "3")
+                    Console.WriteLine("MonDay");
+                else if (DayCounter == "4")
+                    Console.WriteLine("TuesDay");
+                else if (DayCounter == "5")
+                    Console.WriteLine("WednesDay");
+                else if (DayCounter == "6")
+                    Console.WriteLine("ThursDay");
+                else if (DayCounter == "7")
+                    Console.WriteLine("FriDay");
+                else if (DayCounter == "0")
+                {
+                    Console.WriteLine("Bye User");
+                    break;
+                }
+                else
+                    Console.WriteLine("the Input Is Not Valid");
+            }
+        }
+    }
+}
