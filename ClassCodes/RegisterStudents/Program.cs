bfe616acd7f92841f756095375e7b41c49409caa
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:28:37 -0700

    this code will show you the profile of the registerd student

---
+++ ClassCodes/RegisterStudents/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisterStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Enter the Name of The Student");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the LastName of The Student");
+                String lastName = Console.ReadLine();
+                Console.WriteLine("Enter the Id of The Student");
+                string id = Console.ReadLine();
+                Console.WriteLine($"Name:{name},LastName:{lastName},ID:{id}");
+            }
+
+        }
+    }
+}
