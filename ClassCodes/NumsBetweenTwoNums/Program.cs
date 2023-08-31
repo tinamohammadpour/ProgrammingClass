5d6c89399873aa6cb200ae18e56be88c972ab08f
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:21:20 -0700

    this added code will show you the numbers between two input number

---
+++ ClassCodes/NumsBetweenTwoNums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        PrintBetween(num1, num2);
+    }
+
+    private static void PrintBetween(int num1, int num2)
+    {
+        if (num1 <= num2)
+        {
+            for (int i = num1; i <= num2; i++)
+                Console.WriteLine(i);
+        }
+        else
+        {
+            for (int i = num2; i <= num1; i++)
+                Console.WriteLine(i);
+        }
+    }
+}
