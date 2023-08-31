c7f68950a998e9cc469b1c3d09217e0a7d711e13
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 12:40:46 -0700

    the code of a simple calculator has been added

---
+++ ClassCodes/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Input Numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("+:Sum,-:Minus,/:Divide,*:Multiply");
+            string Option =Console.ReadLine();
+
+            if (Option == "+")
+                    Console.WriteLine(num1 + num2);
+            else if (Option == "-")
+                    Console.WriteLine(num1 - num2);
+            else if (Option == "/" && num2 != 0)
+                    Console.WriteLine(num1 / num2);
+            else if (Option == "*")
+                    Console.WriteLine(num1 * num2);
+            
+            else
+                Console.WriteLine("The Input Option is Not Valid");
+
+        }
+    }
+}
