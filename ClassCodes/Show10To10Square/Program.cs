4106737d55d8f13ea2dbdd81e3a1f42f45fab725
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:30:05 -0700

    this code will show you a 10 to 10 square with an interesting pattern

---
+++ ClassCodes/Show10To10Square/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show10To10Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Is A 10*10 Multiplication Table");
+            int Col;
+            int CountRow = 0;
+            for (int Row = 1; Row <= 10; Row++)
+            {
+                Col = Row;
+                while (CountRow != 10)
+                {
+                    Console.Write(Col + " ");
+                    Col++;
+                    CountRow++;
+                    if (Col > 10)
+                        Col = 1;
+                }
+                CountRow = 0;
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
