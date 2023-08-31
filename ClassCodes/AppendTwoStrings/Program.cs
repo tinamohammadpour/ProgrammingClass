75b0f08cb2f763687bfade5f283406535c385389
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 12:36:25 -0700

    the code for appending Two Strings has been added

---
+++ ClassCodes/AppendTwoStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppendTwoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 String So I Append them");
+            string String1 = Console.ReadLine();
+            string String2 = Console.ReadLine();
+            Console.WriteLine((String1 + String2).ToUpper());
+        }
+    }
+}
