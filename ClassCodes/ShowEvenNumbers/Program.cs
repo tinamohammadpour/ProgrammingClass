414f5999cf05ac6dc0951ef7e64c37d4f9ed1807
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:31:40 -0700

    this added code will show you the even numebrs from 1 to 50

---
+++ ClassCodes/ShowEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShowEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is The Program to show you Even Numbers From 1 to 50");
+            Thread.Sleep(2000);
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
