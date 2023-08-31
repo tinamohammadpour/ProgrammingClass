f12825de4891ad9fc9a63d03df443be6dba6a076
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:33:26 -0700

    this added code will show you the 3-digit odd numbers

---
+++ ClassCodes/ShowOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets Show Odd numbers from 100 t0 999");
+            for (int i = 101; i <= 999; i += 2)
+                Console.WriteLine(i);
+
+        }
+    }
+}
