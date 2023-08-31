5f0fa4705a67a278d7a0163ac74c071af4c00943
tinamohammadpour <tina.mpmp2017@gmail.com>
2023-08-31 13:27:24 -0700

    this added code will make you triangle with typing n lines of stars

---
+++ ClassCodes/PrintStar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintStar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of the Lines you Want to print Star in");
+            int LineCounter = Convert.ToInt32(Console.ReadLine());
+            PrintStar(LineCounter);
+        }
+
+        private static void PrintStar(int LineCounter)
+        {
+            for (int i = 0; i <= LineCounter; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
